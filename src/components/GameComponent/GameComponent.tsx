eb2a371ec36954c2d81c0908c9ea597aa01c22ba
MertJSX <mertJSX@proton.me>
2024-10-26 12:14:05 +0300

    Modified GameComponent and GameMapComponents

--- src/components/GameComponent/GameComponent.tsx
+++ src/components/GameComponent/GameComponent.tsx
@@ -6,13 +6,21 @@ import MapFrame from './classes/MapFrame';
 const GameComponent = () => {
   const [map, setMap] = useState<GameMap>(new GameMap());
   const [selectedItem, setSelectedItem] = useState("");
+  const [markedItems, setMarkedItems] = useState<string[]>([]);
   useEffect(() => {
     console.log(map);
   },[map])
 
   return (
     <div>
-      <GameMapComponent gameMap={map} setGameMap={setMap} selectedItem={selectedItem} setSelectedItem={setSelectedItem} />
+      <GameMapComponent 
+      gameMap={map} 
+      setGameMap={setMap} 
+      selectedItem={selectedItem} 
+      setSelectedItem={setSelectedItem}
+      markedItems={markedItems}
+      setMarkedItems={setMarkedItems}
+       />
     </div>
   )
 }
--- src/components/GameComponent/GameMapComponent/GameMapComponent.tsx
+++ src/components/GameComponent/GameMapComponent/GameMapComponent.tsx
@@ -1,15 +1,19 @@
 import React, { useEffect, useRef } from 'react';
 import { GameMap } from '../classes/GameMap';
 import MapFrame from '../classes/MapFrame';
+import { CalculateChessMoves } from '../classes/CalculateChessMoves';
+import { ChessMoveProvider } from '../classes/ChessMoveProvider';
 
 interface GameMapProps {
   gameMap: GameMap,
   setGameMap: React.Dispatch<React.SetStateAction<GameMap>>,
   selectedItem: string,
-  setSelectedItem: React.Dispatch<React.SetStateAction<string>>
+  setSelectedItem: React.Dispatch<React.SetStateAction<string>>,
+  markedItems: string[],
+  setMarkedItems: React.Dispatch<string[]>
 }
 
-const GameMapComponent: React.FC<GameMapProps> = ({ gameMap, setGameMap, selectedItem, setSelectedItem }) => {
+const GameMapComponent: React.FC<GameMapProps> = ({ gameMap, setGameMap, selectedItem, setSelectedItem, markedItems, setMarkedItems }) => {
   const arrayFromGameMap = Array.from(gameMap.mapFrames.values());
   const mapElements = useRef(null)
   useEffect(() => {
@@ -33,10 +37,39 @@ const GameMapComponent: React.FC<GameMapProps> = ({ gameMap, setGameMap, selecte
                     }
                     gameMap.mapFrames.get(item.positionName)?.SetSelected(true);
                     setSelectedItem(item.positionName);
+
+                    ChessMoveProvider.Provide(gameMap, item, markedItems, setMarkedItems)
+
+                    // if (item.piece.name === "Pawn") {
+                    //   let selectedFrame = gameMap.mapFrames.get(item.positionName);
+                    //   if (selectedFrame) {
+                    //     let possibleMoves: Array<string> = CalculateChessMoves.Pawn(gameMap, selectedFrame);
+
+                    //     if (markedItems[0] !== undefined) {
+                    //       markedItems.forEach((markedItem) => {
+                    //         gameMap.mapFrames.get(markedItem)?.SetIsMarked(false);
+                    //       })
+                    //     }
+
+                    //     if (possibleMoves) {
+                    //       possibleMoves.forEach((cordinate) => {
+                    //         gameMap.mapFrames.get(cordinate)?.SetIsMarked(true);
+                    //       })
+                    //     }
+
+                    //     console.log(possibleMoves);
+
+                    //     setMarkedItems(possibleMoves)
+                    //   }
+                    // }
                     setGameMap(gameMap);
                   }
                 }}
-                className={`flex justify-center items-center text-xl w-20 h-20 ${item.isSelected ? "bg-sky-600" : item.color === "black" ? "bg-stone-700" : "bg-gray-500"}`}
+                className={`flex justify-center items-center text-xl w-20 h-20 ${item.isSelected ? "bg-sky-600" :
+                   item.color === "black" && item.isMarked ? "bg-chess-black-green" :
+                   item.color === "white" && item.isMarked ? "bg-chess-white-green" :
+                   item.color === "black" ? "bg-chess-black" : "bg-chess-white"
+                  }`}
                 key={item.positionName}>
                 {item.piece ?
                   <img className='select-none' src={item.piece.image} alt="" width={70} /> : null
