9af00226e601efdaef7c59740720139797de5371
MertJSX <mertJSX@proton.me>
2025-07-18 23:45:51 +0300

    Modify colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,8 @@ module.exports = {
         'chess-black-blue': '#0b5b5c',
         'chess-black-blue-2': '#3d7f80',
         'chess-white': '#6b7280',
-        'chess-white-blue': '#5c6b80',
+        // 'chess-white-blue': '#5c6b80',
+        'chess-white-blue': '#4d596a',
         'chess-white-blue-2': '#7c8da0',
         'chess-point-color': '#3b6e55'
       }
