567415803bf28add254008e4435e2340a0b4c8d9
MertJSX <mertJSX@proton.me>
2025-07-19 23:27:06 +0300

    Improving design: Added new animations to pieces

--- src/components/GameComponent/GameMapComponent/GameMapComponent.tsx
+++ src/components/GameComponent/GameMapComponent/GameMapComponent.tsx
@@ -36,22 +36,22 @@ const GameMapComponent: React.FC<GameMapProps> = ({ gameMap, setGameMap, selecte
 
   return (
     <div>
-      <div
+      <section
         ref={mapElements}
         className={`flex flex-wrap w-[324px] md:w-[644px] m-auto border-2 border-black mt-2`}>
         {
           arrayFromGameMap
             .map((item) => (
-              <div
+              <article
                 onMouseEnter={() => {
-                  if (selectedItem !== "") {
+                  if (selectedItem !== "" && item.isMarked) {
                     setHoveredItem(item.positionName);
                   }
                 }}
                 onMouseLeave={() => {
-                  if (selectedItem !== "") {
+                  // if (selectedItem === "") {
                     setHoveredItem(null);
-                  }
+                  // }
                 }}
                 onClick={(e: React.MouseEvent<HTMLDivElement>) => {
                   if (item.isMarked) {
@@ -69,6 +69,7 @@ const GameMapComponent: React.FC<GameMapProps> = ({ gameMap, setGameMap, selecte
                     setSelectedItem("");
                     setMarkedItems([]);
                     setMarkedPossibleCastles([]);
+                    setHoveredItem(null);
                     chessGame.changeTurn();
                     if (autoFlipBoard) {
                       setFlipBoard(!flipBoard);
@@ -112,9 +113,9 @@ const GameMapComponent: React.FC<GameMapProps> = ({ gameMap, setGameMap, selecte
                         return
                       }
                     }
-                    if (gameMap.isKingInCheck() && chessGame.gameMode !== "sandbox") {
-                      return
-                    }
+                    // if (gameMap.isKingInCheck() && chessGame.gameMode !== "sandbox" && item.piece.name !== "King") {
+                    //   return
+                    // }
                     gameMap.mapFrames.get(selectedItem)?.SetSelected(false);
                     markedItems.forEach((markedFrameID) => {
                       gameMap.mapFrames.get(markedFrameID)?.SetIsMarked(false);
@@ -141,16 +142,17 @@ const GameMapComponent: React.FC<GameMapProps> = ({ gameMap, setGameMap, selecte
                     setSelectedItem("");
                     setMarkedItems([]);
                     setMarkedPossibleCastles([]);
+                    setHoveredItem(null);
                     setGameMap(gameMap);
                   }
                 }}
-                className={`relative flex justify-center items-center text-xl w-10 md:w-20 h-10 md:h-20
-                  ${item.isSelected ? "bg-sky-600" :
-                  item.isThreatened ? `bg-red-600` :
+                className={`frame relative flex justify-center items-center text-xl w-10 md:w-20 h-10 md:h-20 transition-all
+                  ${item.isSelected ? "bg-sky-600 selected-frame" :
+                  item.isThreatened ? `bg-red-600 checked-frame` :
                   item.color === "black" && (item.isMarked || item.isMarkedForCastle) ? `${item.piece ? "bg-chess-black-blue-2 hover:bg-sky-500" : "hover:bg-chess-black-blue"} bg-chess-black` :
                     item.color === "white" && (item.isMarked || item.isMarkedForCastle) ? `${item.piece ? "bg-chess-white-blue-2 hover:bg-sky-500" : "hover:bg-chess-white-blue"} bg-chess-white` :
                       item.color === "black" ? "bg-chess-black" : "bg-chess-white"
-                  }`}
+                  } ${item.positionName === hoveredItem ? "cursor-pointer" : null}`}
                 key={item.positionName}>
                 {
                   colPlaceForCoordinate(item, flipBoard) ?
@@ -167,15 +169,15 @@ const GameMapComponent: React.FC<GameMapProps> = ({ gameMap, setGameMap, selecte
                   </h1> : null
                 }
                 {item.piece ?
-                  <img className='select-none z-10' src={item.piece.image} alt="" width={70} /> : item.isMarked && hoveredItem !== item.positionName ?
+                  <img className='select-none z-10 transition-all' src={item.piece.image} alt="" width={70} /> : item.isMarked && hoveredItem !== item.positionName ?
                     <TbPointFilled
                    size={55} className="text-cyan-500" />
                   : null
                 }
-              </div>
+              </article>
             ))
         }
-      </div>
+      </section>
       {chessGame.gameMode === "1v1" ?
         <h1 className="text-center text-white text-2xl">
           {chessGame.turn === "black" ? 
